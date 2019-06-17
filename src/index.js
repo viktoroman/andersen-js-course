24df070c45db681a038239b6a1e9ed8d92cbb824
viktorom <viktor28rom@gmail.com>
2019-06-17 19:18:56 +0300

    Controller using

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './styles/main.css';
+// import Item from './model/Item';
+// import Recipe from './model/Recipe';
+import CraftSpaceController from './controller/CraftTableController';
 
-console.log('Hi');
+// tests
+/*
+import testModelClasses from './test/testModelClasses';
+
+testModelClasses();
+*/
+
+// tests
+/*
+import testInitDefaultEntities from './test/testInitDefaultEntities';
+
+testInitDefaultEntities();
+*/
+
+const craftSpaceController = new CraftSpaceController();
+craftSpaceController.initDefaultEntities();
