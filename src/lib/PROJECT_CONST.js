0b598c93f956e93e9471ff2d8f6ea5b8d1f71139
viktorom <viktor28rom@gmail.com>
2019-06-22 13:28:39 +0300

    Add default recipies

--- src/lib/PROJECT_CONST.js
+++ src/lib/PROJECT_CONST.js
@@ -22,8 +22,8 @@ const PROJECT_CONST = (() => {
     ITEM_ALREADY_EXISTS: 'This item is already there!!',
     RECIPE_ALREADY_EXISTS: 'This recipe is already there!!',
     RECIPE_WRONG: 'Recipe is wrong!!',
-    ITEM_CRAFTED: 'New item is created!!',
-    RECIPE_CRAFTED: 'New recipe is created!!',
+    ITEM_CRAFTED: 'New item has been created!!',
+    RECIPE_CRAFTED: 'New recipe has been created!!',
   };
 
   // Entity constants
@@ -35,23 +35,89 @@ const PROJECT_CONST = (() => {
   };
 
   // set of
-  const utilityDefaultEntities = {
-    item: [
-      new Item('Wood', 'Wood'),
-      new Item('Iron', 'Iron'),
-      new Item('Glass', 'Glass'),
-      new Item('Textile', 'Textile'),
-    ],
-    recipe: [
-      new Recipe(new Item('Chair', 'Chair'), new Item('Wood', 'Wood'), new Item('Iron', 'Iron')),
-      new Recipe(
-        new Item('Table', 'Table'),
-        new Item('Wood', 'Wood'),
-        new Item('Wood', 'Wood'),
-        new Item('Iron', 'Iron')
-      ),
-    ],
-  };
+  const utilityDefaultEntities = (() => {
+    // basic items
+    const wood = new Item('Древесина', 'Единица древесины');
+    const iron = new Item('Метал', 'Единица метала');
+    const glass = new Item('Стекло', 'Единица стекла');
+    const textile = new Item('Ткань', 'Единица ткани');
+
+    // complex items
+    const chair = new Item('Стул', 'Деревянный стул');
+    const table = new Item('Стол', 'Деревянный стол');
+    const wndw = new Item('Окно', 'Окно с деревянной рамой');
+    const armChair = new Item('Кресло', 'Кресло');
+    const sofa = new Item('Диван', 'Диван');
+    const moderTable = new Item('Стильный стол', 'Моднейший стол со стеклянным покрытием');
+    const door = new Item('Дверь', 'Деревянная дверь');
+    const ironDoor = new Item('Стальная дверь', 'Прочная стальная дверь');
+    const mirror = new Item('Зеркало', 'Зеркало');
+    const wardrobe = new Item('Шкаф', 'Вместительный гардеробный шкаф');
+    const fullSet = new Item('Мебельный сет', 'Набор мебели');
+    const bagWithBrokenGlass = new Item(
+      'Мешок с битым стеклом',
+      'Абсолютно бесполезная вещь. Зачем он - неизвестно. Но можно поставить себе в комнату и говорить гостям, что это моднейший арт объект, а Вы - величайший ценитель-эстет современности'
+    );
+
+    const chairRecipe = new Recipe(chair.clone(), wood.clone(), iron.clone());
+    const tableRecipe = new Recipe(table.clone(), wood.clone(), wood.clone(), iron.clone());
+    const wndwRecipe = new Recipe(wndw.clone(), wood.clone(), glass.clone(), glass.clone());
+    const armChairRecipe = new Recipe(armChair.clone(), chair.clone(), textile.clone());
+    const sofaRecipe = new Recipe(
+      sofa.clone(),
+      wood.clone(),
+      wood.clone(),
+      iron.clone(),
+      textile.clone(),
+      textile.clone()
+    );
+    const moderTableRecipe = new Recipe(moderTable.clone(), table.clone(), glass.clone());
+    const doorRecipe = new Recipe(door.clone(), wood.clone(), wood.clone(), iron.clone());
+    const ironDoorRecipe = new Recipe(ironDoor.clone(), door.clone(), iron.clone(), iron.clone());
+    const mirrorRecipe = new Recipe(mirror.clone(), wood.clone(), glass.clone(), iron.clone());
+    const wardrobeRecipe = new Recipe(
+      wardrobe.clone(),
+      mirror.clone(),
+      wood.clone(),
+      wood.clone(),
+      wood.clone(),
+      iron.clone(),
+      iron.clone()
+    );
+    const fullSetRecipe = new Recipe(
+      fullSet.clone(),
+      chair.clone(),
+      table.clone(),
+      armChair.clone(),
+      sofa.clone(),
+      moderTable.clone(),
+      mirror.clone(),
+      wardrobe.clone()
+    );
+    const bagWithBrokenGlassRecipe = new Recipe(
+      bagWithBrokenGlass.clone(),
+      textile.clone(),
+      glass.clone()
+    );
+
+    return {
+      item: [wood, iron, glass, textile],
+      recipe: [
+        chairRecipe,
+        tableRecipe,
+        wndwRecipe,
+        armChairRecipe,
+        sofaRecipe,
+        moderTableRecipe,
+        doorRecipe,
+        ironDoorRecipe,
+        mirrorRecipe,
+        wardrobeRecipe,
+        fullSetRecipe,
+        bagWithBrokenGlassRecipe,
+      ],
+    };
+  })();
 
   return {
     eventMessages: utilityEventMessages,
