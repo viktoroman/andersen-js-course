a1a185ebd7bb63a18b1d2e5a346317e49d1176e3
viktorom <viktor28rom@gmail.com>
2019-06-25 21:45:04 +0300

    task_001 is done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-import './styles/main.css';
+import { foo, createCb } from './asynchronous-programming/task_001';
 
-console.log('Hi');
+console.log('task_001 =========================');
+foo(-10, createCb('value -10'));
+foo(0, createCb('value 0'));
+foo(10, createCb('value 10'));
+foo(20, createCb('value 20'));
