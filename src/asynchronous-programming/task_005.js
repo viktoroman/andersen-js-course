92c8f909dacf17a429140c1e8f7b7440542c1304
viktorom <viktor28rom@gmail.com>
2019-06-26 22:53:34 +0300

    task_005. Fix

--- src/asynchronous-programming/task_005.js
+++ src/asynchronous-programming/task_005.js
@@ -1,4 +1,4 @@
-const urls = [
+const urlsTask005 = [
   `http://www.json-generator.com/api/json/get/cevhxOsZnS`,
   `http://www.json-generator.com/api/json/get/cguaPsRxAi`,
   `http://www.json-generator.com/api/json/get/cfDZdmxnDm`,
@@ -10,8 +10,27 @@ const getData = url => {
   return fetch(url).then(resp => resp.json());
 };
 
-const getDataAll = () => {
+const getDataAllParallel = urls => {
   Promise.all(urls.map(url => getData(url))).then(console.log);
 };
 
-export default getDataAll;
+const getDataAllSequentail = urls => {
+  const res = [];
+
+  urls
+    .map(url => getData(url))
+    .reduce((prevProm, currProm) => {
+      return prevProm.then(data => {
+        if (data) {
+          res.push(data);
+        }
+        return currProm;
+      });
+    }, Promise.resolve())
+    .then(data => {
+      res.push(data);
+      console.log(res);
+    });
+};
+
+export { urlsTask005, getDataAllParallel, getDataAllSequentail };
