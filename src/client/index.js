2e2d5e29e61cb36ef1daf2bb362dc48ca899e599
viktorom <viktor28rom@gmail.com>
2019-07-04 00:14:28 +0300

    Using controller component for main page.

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,10 @@
 import './styles/main.css';
 
-console.log('Hi');
+// import testREST from './test/testREST';
+
+// testREST();
+
+import UserController from './controller/UserController';
+
+// eslint-disable-next-line no-unused-vars
+const userController = new UserController();
