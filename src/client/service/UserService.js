da8ff0fb4ce557563e1e9aa72cdf5fb01eec9ca3
viktorom <viktor28rom@gmail.com>
2019-07-06 02:24:45 +0300

    Delete extra code and comments from client side service.

--- src/client/service/UserService.js
+++ src/client/service/UserService.js
@@ -3,63 +3,9 @@ import CRUD from './CRUD';
 import GLOBAL from '../../lib/GLOBAL';
 
 const UserService = (() => {
-  // // get all users
-  // const getAll = async () => {
-  //   try {
-  //     const userData = await fetch(`${GLOBAL.USER_REST_ROOT_URL}/all`).then(r => r.json());
-  //     return userData.map(v => new User(v));
-  //   } catch (error) {
-  //     throw new Error(`Get all user error: ${error}`);
-  //   }
-  // };
-
-  // // insert user
-  // const insert = async user => {
-  //   try {
-  //     const insData = await fetch(GLOBAL.USER_REST_ROOT_URL, {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify(user),
-  //     }).then(r => r.json());
-  //     return new User(insData);
-  //   } catch (error) {
-  //     throw new Error(`Insert user error ${error}`);
-  //   }
-  // };
-
-  // // update
-  // const update = async user => {
-  //   try {
-  //     const updData = await fetch(GLOBAL.USER_REST_ROOT_URL, {
-  //       method: 'PUT',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify(user),
-  //     }).then(r => r.json());
-
-  //     return new User(updData);
-  //   } catch (error) {
-  //     throw new Error(`Update user error: ${error}`);
-  //   }
-  // };
-
-  // // delete
-  // const deleteById = async userId => {
-  //   try {
-  //     const delData = await fetch(`${GLOBAL.USER_REST_ROOT_URL}/${userId}`, {
-  //       method: 'DELETE',
-  //       headers: { 'Content-Type': 'application/json' },
-  //     }).then(r => r.json());
-
-  //     return new User(delData);
-  //   } catch (error) {
-  //     throw new Error(`Delete user error: ${error}`);
-  //   }
-  // };
-
   // get all users
   const userGetAll = async () => {
     try {
-      // return await CRUD.getAll(`${GLOBAL.USER_REST_ROOT_URL}/all`).map(v => new User(v));
       const result = await CRUD.getAll(`${GLOBAL.USER_REST_ROOT_URL}/all`);
       return result.map(v => new User(v));
     } catch (error) {
